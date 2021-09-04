88d9a32d1acbb8e90399ea239be06c118c4782e3
GeekerHungary <90099719+GeekerHungary@users.noreply.github.com>
2021-09-04 16:42:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const prefix = '-';
+
+client.on('ready', () => {
+  console.log('Online!');
+  client.user.setActivity('-ping', {type: 'PLAYING'});
+});
+
+client.on('messageCreate', async (message) => {
+if(message.author.bot) { return; }
+const args = message.content.slice(prefix.length).split(/ +/g);
+
+// -- SIMPLE COMMANDS
+
+if(message.content === `${prefix}ping`) {
+  message.reply('Pong!');
+}
+
+// -- EMBEDS
+if(message.content === '-embed') {
+  const embed = new MessageEmbed()
+  .setTitle('Cím')
+  .setDescription('Leírás')
+  .addField('Alcím címe', 'Alcím értéke')
+  .setTimestamp()
+  .setColor('BLUE')
+  message.channel.send({ embeds: [embed] });
+}
+});
+
+client.login('process.env.BOT_TOKEN');
